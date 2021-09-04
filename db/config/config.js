1a2dfd8df8ec67770f09c8505e1da61dc7a68cc3
salehalqattan <salehalqattan@outlook.com>
2021-09-04 11:00:53 +0300

    fix the production object to the database assigned to the server

--- db/config/config.js
+++ db/config/config.js
@@ -16,10 +16,5 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
   },
 };
