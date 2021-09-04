bad93ffb393933b03dc17fe3d912b65c1ceca4ab
salehalqattan <salehalqattan@outlook.com>
2021-09-04 09:30:47 +0300

    setting env

---
+++ db/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: null,
+    database: process.env.DATABASE_NAME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: false,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
